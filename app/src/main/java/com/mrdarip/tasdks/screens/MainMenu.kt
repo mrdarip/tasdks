81be0d6f2f4b5c7939515bfa8095559eb2c3a31c
mrdarip <darioelas2005@gmail.com>
2024-04-09 13:35:54 +0200

    solving Scaffold issues

--- app/src/main/java/com/mrdarip/tasdks/screens/MainMenu.kt
+++ app/src/main/java/com/mrdarip/tasdks/screens/MainMenu.kt
@@ -1,33 +1,71 @@
 package com.mrdarip.tasdks.screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun MainMenu(){
-    Scaffold{
-        BodyContent()
+fun MainMenu() {
+    Scaffold(topBar = {
+        Text(text = "hello")
+    }, bottomBar = {
+        BottomAppBar(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            contentColor = MaterialTheme.colorScheme.primary,
+        ) {
+            Text(
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.Center,
+                text = "Bottom app bar",
+            )
+        }
+    }, floatingActionButton = {
+        FloatingActionButton(onClick = { }) {
+            Icon(Icons.Default.Add, contentDescription = "Add")
+        }
+    }) { innerPadding ->
+        Column(
+            modifier = Modifier.padding(innerPadding),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+        ) {
+            BodyContent()
+        }
     }
 }
 
 @Composable
-fun BodyContent(){
+fun BodyContent() {
     Column {
         Text(text = "hola")
         Button(onClick = { /*TODO*/ }) {
-            Text(text = "peo")
+            Text(text = "test")
         }
     }
 }
 
 @Preview
 @Composable
-fun DefaultPreview(){
+fun DefaultPreview() {
     MainMenu()
 }
