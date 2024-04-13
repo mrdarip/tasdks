687894e4edacf66547110a6221759a12f5ade989
mrdarip <darioelas2005@gmail.com>
2024-04-13 17:51:11 +0200

    renamed some entities properties, made nullable some others and formatted Playlist.kt

--- app/src/main/java/com/mrdarip/tasdks/data/entity/Playlist.kt
+++ app/src/main/java/com/mrdarip/tasdks/data/entity/Playlist.kt
@@ -1,5 +1,6 @@
 package com.mrdarip.tasdks.data.entity
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import java.util.Date
@@ -7,79 +8,69 @@ import java.util.Date
 
 @Entity(tableName = "Tasks")
 data class Task(
-    @PrimaryKey(autoGenerate = true)
-    val taskId: Long = 0,
+    @PrimaryKey(autoGenerate = true) val taskId: Long = 0,
     val name: String,
-    val comment: String,
-    val placeId: Long
-){}
+    val comment: String?,
+    val placeId: Long?
+) {}
 
 @Entity(tableName = "Places")
 data class Place(
-    @PrimaryKey(autoGenerate = true)
-    val placeId: Long = 0,
+    @PrimaryKey(autoGenerate = true) val placeId: Long = 0,
     val name: String,
-    val parentPlaceId: Long
-){}
+    val parentPlaceId: Long?
+) {}
 
 @Entity(tableName = "Objects")
 data class Object(
-    @PrimaryKey(autoGenerate = true)
-    val objectId: Long = 0,
+    @PrimaryKey(autoGenerate = true) val objectId: Long = 0,
     val name: String,
-    val placeId: Long
-){}
+    val placeId: Long?
+) {}
 
 @Entity(tableName = "Activators")
 data class Activator(
-    @PrimaryKey(autoGenerate = true)
-    val activatorId: Long = 0,
-    val comment: String,
+    @PrimaryKey(autoGenerate = true) val activatorId: Long = 0,
+    val comment: String?,
     val minRepSec: UInt,
-    val optRepSec: UInt,
-    val maxRepSec: UInt,
-    val endAfterDate: Date,
-    val endAfterRep: UInt,
-    val userCancelled: Bool
-){}
+    val optRepSec: UInt?,
+    val maxRepSec: UInt?,
+    val endAfterDate: Date?,
+    val endAfterRep: UInt?,
+    @ColumnInfo(defaultValue = "0") val userCancelled: Boolean
+) {}
 
 @Entity(tableName = "Executions")
 data class Execution(
-    @PrimaryKey(autoGenerate = true)
-    val executionId: Long = 0,
-    val startDate: Date,
-    val endDate: Date,
+    @PrimaryKey(autoGenerate = true) val executionId: Long = 0,
+    val start: Date,
+    val end: Date,
+    val successfullyEnded: Boolean,
     val activatorId: Long,
-    val resourceId: Long
-){}
+    val resourceId: Long?
+) {}
 
 enum class ResourceType {
-    MUSIC,AUDIOBOOK,PODCAST
+    MUSIC, AUDIOBOOK, PODCAST, VIDEO
 }
 
 @Entity(tableName = "Resources")
 data class Resource(
-    @PrimaryKey(autoGenerate = true)
-    val resourceId: Long = 0,
+    @PrimaryKey(autoGenerate = true) val resourceId: Long = 0,
     val name: String,
     val resourceType: ResourceType
-){}
+) {}
 
 
-
-
-
-
-
-@Entity()
+@Entity(primaryKeys = ["taskId", "objectId"])
 data class TaskObjectCR(
-    val playlistId: Long,
-    val taskId: Long
-){}
+    val taskId: Long,
+    val objectId: Long
+) {}
 
-@Entity()
+@Entity(primaryKeys = ["parentTaskId", "childTaskId"])
 data class TaskParentCR(
-    val playlistId: Long,
-    val taskId: Long
-){}
+    val parentTaskId: Long,
+    val childTaskId: Long
+) {}
 
