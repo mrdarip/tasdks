7f7d5a8dd4109dbf3234552c0575097b2635359a
mrdarip <darioelas2005@gmail.com>
2024-04-13 19:43:03 +0200

    created sample dao and database builder

---
+++ app/src/main/java/com/mrdarip/tasdks/data/database/RoomDatabase.kt
@@ -0,0 +1,35 @@
+package com.mrdarip.tasdks.data.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.mrdarip.tasdks.data.entity.Activator
+import com.mrdarip.tasdks.data.entity.Execution
+import com.mrdarip.tasdks.data.entity.Object
+import com.mrdarip.tasdks.data.entity.Place
+import com.mrdarip.tasdks.data.entity.Resource
+import com.mrdarip.tasdks.data.entity.Task
+import com.mrdarip.tasdks.data.entity.TaskDAO
+import com.mrdarip.tasdks.data.entity.TaskObjectCR
+import com.mrdarip.tasdks.data.entity.TaskTaskCR
+
+@Database(entities = arrayOf(Task::class, Place::class, Object::class, Activator::class,Execution::class,Resource::class,TaskTaskCR::class, TaskObjectCR::class), version = 1, exportSchema = false)
+public abstract class TasdksDatabase : RoomDatabase() {
+
+    abstract fun taskDao(): TaskDAO
+
+    companion object {
+        @Volatile
+        private var Instance: TasdksDatabase? = null
+
+        fun getDatabase(context: Context): TasdksDatabase {
+            // if the Instance is not null, return it, otherwise create a new database instance.
+            return Instance ?: synchronized(this) {
+                Room.databaseBuilder(context, TasdksDatabase::class.java, "item_database")
+                    .build()
+                    .also { Instance = it }
+            }
+        }
+    }
+}
--- app/src/main/java/com/mrdarip/tasdks/data/entity/Playlist.kt
+++ app/src/main/java/com/mrdarip/tasdks/data/entity/Playlist.kt
@@ -1,34 +1,44 @@
 package com.mrdarip.tasdks.data.entity
 
 import androidx.room.ColumnInfo
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Embedded
 import androidx.room.Entity
+import androidx.room.Insert
+import androidx.room.Junction
+import androidx.room.OnConflictStrategy
 import androidx.room.PrimaryKey
+import androidx.room.Query
+import androidx.room.Relation
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
 import java.util.Date
 
 
-@Entity(tableName = "Tasks")
+@Entity(tableName = "tasks")
 data class Task(
     @PrimaryKey(autoGenerate = true) val taskId: Long = 0,
     val name: String,
     val comment: String?,
     val placeId: Long?
-) {}
+)
 
-@Entity(tableName = "Places")
+@Entity(tableName = "places")
 data class Place(
     @PrimaryKey(autoGenerate = true) val placeId: Long = 0,
     val name: String,
     val parentPlaceId: Long?
-) {}
+)
 
-@Entity(tableName = "Objects")
+@Entity(tableName = "objects")
 data class Object(
     @PrimaryKey(autoGenerate = true) val objectId: Long = 0,
     val name: String,
     val placeId: Long?
-) {}
+)
 
-@Entity(tableName = "Activators")
+@Entity(tableName = "activators")
 data class Activator(
     @PrimaryKey(autoGenerate = true) val activatorId: Long = 0,
     val comment: String?,
@@ -38,9 +48,9 @@ data class Activator(
     val endAfterDate: Date?,
     val endAfterRep: UInt?,
     @ColumnInfo(defaultValue = "0") val userCancelled: Boolean
-) {}
+)
 
-@Entity(tableName = "Executions")
+@Entity(tableName = "executions")
 data class Execution(
     @PrimaryKey(autoGenerate = true) val executionId: Long = 0,
     val start: Date,
@@ -48,29 +58,56 @@ data class Execution(
     val successfullyEnded: Boolean,
     val activatorId: Long,
     val resourceId: Long?
-) {}
+)
 
 enum class ResourceType {
     MUSIC, AUDIOBOOK, PODCAST, VIDEO
 }
 
-@Entity(tableName = "Resources")
+@Entity(tableName = "resources")
 data class Resource(
     @PrimaryKey(autoGenerate = true) val resourceId: Long = 0,
     val name: String,
     val resourceType: ResourceType
-) {}
+)
 
 
 @Entity(primaryKeys = ["taskId", "objectId"])
 data class TaskObjectCR(
     val taskId: Long,
     val objectId: Long
-) {}
+)
 
 @Entity(primaryKeys = ["parentTaskId", "childTaskId"])
-data class TaskParentCR(
+data class TaskTaskCR(
     val parentTaskId: Long,
     val childTaskId: Long
-) {}
+)
 
+data class TasksByTask(
+    @Embedded val task: Task,
+    @Relation(
+        parentColumn = "parentTaskId",
+        entityColumn = "childTaskId",
+        associateBy = Junction(TaskTaskCR::class)
+    )
+    val songs: List<Task>
+)
+
+@Dao
+interface TaskDAO {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(task : Task)
+
+    @Update
+    suspend fun update(item: Task)
+
+    @Delete
+    suspend fun delete(item: Task)
+
+    @Query("SELECT * from tasks WHERE taskId = :id")
+    fun getItem(id: Int): Flow<Task>
+
+    @Query("SELECT * from tasks ORDER BY name ASC")
+    fun getAllItems(): Flow<List<Task>>
+}
