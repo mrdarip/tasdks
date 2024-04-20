2ae53b5cfa8d5fa790193b3343cc299be6095b2e
mrdarip <darioelas2005@gmail.com>
2024-04-20 02:13:47 +0200

    Now we have a NavigationDrawer to fill

--- app/src/main/java/com/mrdarip/tasdks/screens/MainMenu.kt
+++ app/src/main/java/com/mrdarip/tasdks/screens/MainMenu.kt
@@ -1,6 +1,5 @@
 package com.mrdarip.tasdks.screens
 
-import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -20,31 +19,36 @@ import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.mrdarip.tasdks.data.entity.Task
+import kotlinx.coroutines.launch
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -53,61 +57,73 @@ fun MainMenu(navController: NavController) {
     val mainMenuViewModel = viewModel(modelClass = MainMenuViewModel::class.java)
     val mainMenuState = mainMenuViewModel.state
 
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
 
-    Text(
-        text = "Hello World!"
-    )
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                colors = topAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primaryContainer,
-                    titleContentColor = MaterialTheme.colorScheme.primary,
-                ),
-                title = {
-                    Text("Top app bar")
-                },
-                navigationIcon = {
-                    IconButton(onClick = { /*TODO*/ }) {
-                        Icon(
-                            Icons.Filled.Home,
-                            contentDescription = ""
-                        )
-                    }
-                }, actions = {
-                    IconButton(onClick = { /*TODO*/ }) {
-                        Icon(
-                            Icons.Filled.AccountCircle,
-                            contentDescription = ""
-                        )
-                    }
-                })
-        }, bottomBar = {
-            var selectedItem by remember { mutableIntStateOf(0) }
-            val items = listOf("MainMenu", "search", "Stats")
 
-            NavigationBar {
-                items.forEachIndexed { index, item ->
-                    NavigationBarItem(
-                        icon = { Icon(Icons.Filled.Favorite, contentDescription = item) },
-                        label = { Text(item) },
-                        selected = selectedItem == index,
-                        onClick = { selectedItem = index }
-                    )
+
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            ModalDrawerSheet {
+                Text(text = "paracetamol")
+            }
+        },
+        content = {
+            Scaffold(
+                topBar = {
+                    TopAppBar(
+                        colors = topAppBarColors(
+                            containerColor = MaterialTheme.colorScheme.primaryContainer,
+                            titleContentColor = MaterialTheme.colorScheme.primary,
+                        ),
+                        title = {
+                            Text("Top app bar")
+                        },
+                        navigationIcon = {
+                            IconButton(onClick = {scope.launch { drawerState.open() }}) {
+                                Icon(
+                                    Icons.Filled.Home,
+                                    contentDescription = ""
+                                )
+                            }
+                        }, actions = {
+                            IconButton(onClick = { /*TODO*/ }) {
+                                Icon(
+                                    Icons.Filled.AccountCircle,
+                                    contentDescription = ""
+                                )
+                            }
+                        })
+                }, bottomBar = {
+                    var selectedItem by remember { mutableIntStateOf(0) }
+                    val items = listOf("MainMenu", "search", "Stats")
+
+                    NavigationBar {
+                        items.forEachIndexed { index, item ->
+                            NavigationBarItem(
+                                icon = { Icon(Icons.Filled.Favorite, contentDescription = item) },
+                                label = { Text(item) },
+                                selected = selectedItem == index,
+                                onClick = { selectedItem = index }
+                            )
+                        }
+                    }
+                }, floatingActionButton = {
+                    FloatingActionButton(onClick = {}) {
+                        Icon(Icons.Default.PlayArrow, contentDescription = "Add")
+                    }
+                }) { innerPadding ->
+                Column(
+                    modifier = Modifier.padding(innerPadding),
+                    verticalArrangement = Arrangement.spacedBy(16.dp),
+                ) {
+                    BodyContent(mainMenuViewModel, mainMenuState)
                 }
             }
-        }, floatingActionButton = {
-            FloatingActionButton(onClick = {}) {
-                Icon(Icons.Default.PlayArrow, contentDescription = "Add")
-            }
-        }) { innerPadding ->
-        Column(
-            modifier = Modifier.padding(innerPadding),
-            verticalArrangement = Arrangement.spacedBy(16.dp),
-        ) {
-            BodyContent(mainMenuViewModel, mainMenuState)
         }
-    }
+    )
+
 }
 
 @Composable
@@ -126,14 +142,14 @@ fun BodyContent(mainMenuViewModel: MainMenuViewModel, mainMenuState: MainMenuSta
 
 @Composable
 fun TaskCard(task: Task, mainMenuViewModel: MainMenuViewModel, onClick: () -> Unit = {}) {
-
     val placeName by mainMenuViewModel.getPlaceName(task.placeId).collectAsState(initial = "")
-    Log.d("hola",placeName)
+
     Column(
         verticalArrangement = Arrangement.Top, modifier = Modifier
             .width(150.dp)
             .height(120.dp)
-            .clip(RoundedCornerShape(12.dp)).background(MaterialTheme.colorScheme.primaryContainer)
+            .clip(RoundedCornerShape(12.dp))
+            .background(MaterialTheme.colorScheme.primaryContainer)
             .clickable(onClick = onClick)
             .padding(16.dp)
     ) {
@@ -168,12 +184,16 @@ fun TasksCardRow(tasks: List<Task>, title: String, mainMenuViewModel: MainMenuVi
         style = MaterialTheme.typography.headlineMedium,
         color = MaterialTheme.colorScheme.onSurfaceVariant,
         modifier = Modifier
-            .padding(16.dp, 32.dp,16.dp,8.dp)
+            .padding(16.dp, 32.dp, 16.dp, 8.dp)
             .fillMaxWidth()
 
     )
 
-    LazyRow(modifier = Modifier.padding(0.dp, 8.dp), contentPadding = PaddingValues(horizontal = 16.dp),horizontalArrangement = Arrangement.spacedBy(16.dp)){
+    LazyRow(
+        modifier = Modifier.padding(0.dp, 8.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        horizontalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
         items(tasks) { task ->
             TaskCard(
                 task = task,
@@ -184,19 +204,4 @@ fun TasksCardRow(tasks: List<Task>, title: String, mainMenuViewModel: MainMenuVi
             )
         }
     }
-}
-
-@Preview
-@Composable
-fun test(){
-    Text(
-        text = "Hola mundo",
-        style = MaterialTheme.typography.headlineMedium,
-        color = MaterialTheme.colorScheme.onSurfaceVariant,
-        modifier = Modifier
-            .padding(8.dp, 16.dp,8.dp,8.dp)
-            .fillMaxWidth()
-            .height(44.dp)
-
-    )
 }
