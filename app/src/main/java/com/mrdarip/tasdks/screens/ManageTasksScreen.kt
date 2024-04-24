5a5e756980cad3113a932eaaac91f35b943cd5c6
mrdarip <darioelas2005@gmail.com>
2024-04-24 12:32:16 +0200

    Added a Card composable to TaskLiItem, created a preview for it an centered the emoji icon vertically

--- app/src/main/java/com/mrdarip/tasdks/screens/ManageTasksScreen.kt
+++ app/src/main/java/com/mrdarip/tasdks/screens/ManageTasksScreen.kt
@@ -9,13 +9,16 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
@@ -51,23 +54,20 @@ fun ManageTasksBodyContent(mainMenuViewModel: MainMenuViewModel, mainMenuState: 
 
 @Composable
 fun TaskLiItem(task: Task, placeName: String, onClick: () -> Unit = {}) {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .clickable(onClick = onClick),
-        horizontalArrangement = Arrangement.SpaceBetween
-    ) {
-        Row {
+    Card(modifier = Modifier.fillMaxWidth().clickable(onClick = onClick)) {// TODO: Add task min/opt/max
+        Row(horizontalArrangement = Arrangement.spacedBy(space = 8.dp), modifier = Modifier.padding(16.dp)){
             Text(
                 task.iconEmoji ?: "🤕",
-                style = MaterialTheme.typography.headlineSmall
+                style = MaterialTheme.typography.headlineSmall,
+                color = MaterialTheme.colorScheme.onTertiaryContainer,
+                modifier = Modifier.align(Alignment.CenterVertically)
             )
             Column {
                 Text(
                     text = task.name,
                     maxLines = 1,
                     overflow = TextOverflow.Ellipsis,
-                    style = MaterialTheme.typography.headlineMedium
+                    style = MaterialTheme.typography.headlineSmall
                 )
                 Text(
                     text = task.comment ?: "NO DESCRIPTION",
@@ -83,6 +83,19 @@ fun TaskLiItem(task: Task, placeName: String, onClick: () -> Unit = {}) {
                 )
             }
         }
-
     }
+}
+
+@Preview
+@Composable
+fun TaskLiItemPreview() {
+    TaskLiItem(
+        task = Task(
+            name = "Task name",
+            comment = "Task comment",
+            iconEmoji = "🐱",
+            placeId = null
+        ),
+        placeName = "Place name"
+    )
 }
