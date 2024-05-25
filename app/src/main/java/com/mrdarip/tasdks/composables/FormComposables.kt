c60bf4c0fabe37feacc060e81041f94b95acc899
mrdarip <darioelas2005@gmail.com>
2024-05-26 01:09:27 +0200

    Implemented repetition time unit selection

--- app/src/main/java/com/mrdarip/tasdks/composables/FormComposables.kt
+++ app/src/main/java/com/mrdarip/tasdks/composables/FormComposables.kt
@@ -14,10 +14,16 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.Button
 import androidx.compose.material3.DatePicker
 import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -25,14 +31,21 @@ import androidx.compose.material3.TextField
 import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.toSize
 import com.mrdarip.tasdks.data.entity.Activator
+import com.mrdarip.tasdks.data.entity.RepetitionType
 import com.mrdarip.tasdks.data.entity.Task
 
 
@@ -166,6 +179,65 @@ fun ActivatorFields(
                 modifier = Modifier.weight(1f)
             )
         }
+
+        var mExpanded by remember { mutableStateOf(false) }
+
+        // Create a list of cities
+        val mCities: List<String> = RepetitionType.entries.map { it.name }
+
+        // Create a string value to store the selected city
+        var mSelectedText by remember { mutableStateOf("SELECT REPETITION TYPE") }
+
+        var mTextFieldSize by remember { mutableStateOf(Size.Zero) }
+
+        // Up Icon when expanded and down icon when collapsed
+        val icon = if (mExpanded)
+            Icons.Filled.KeyboardArrowUp
+        else
+            Icons.Filled.KeyboardArrowDown
+
+        Column(Modifier.padding(20.dp)) {
+
+            // Create an Outlined Text Field
+            // with icon and not expanded
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .onGloballyPositioned { coordinates ->
+                        // This value is used to assign to
+                        // the DropDown the same width
+                        mTextFieldSize = coordinates.size.toSize()
+                    },
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(mSelectedText)
+                Icon(icon, "contentDescription",
+                    Modifier.clickable { mExpanded = !mExpanded })
+            }
+
+            // Create a drop-down menu with list of cities,
+            // when clicked, set the Text Field text as the city selected
+            DropdownMenu(
+                expanded = mExpanded,
+                onDismissRequest = { mExpanded = false },
+                modifier = Modifier
+                    .width(with(LocalDensity.current) { mTextFieldSize.width.toDp() })
+            ) {
+                mCities.forEach { label ->
+                    DropdownMenuItem(onClick = {
+                        mSelectedText = label
+                        mExpanded = false
+                    }, text = {
+                        Text(text = label)
+                    })
+                }
+            }
+        }
+
+
+
+
+
 
         val openDialog = remember { mutableStateOf(false) }
         Button(
