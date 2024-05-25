8bece997559bcf28c6ed48557585b0ea877ea3b8
mrdarip <darioelas2005@gmail.com>
2024-05-25 19:40:57 +0200

    Fixed getActiveActivators Query

--- app/src/main/java/com/mrdarip/tasdks/data/entity/DAOs.kt
+++ app/src/main/java/com/mrdarip/tasdks/data/entity/DAOs.kt
@@ -147,7 +147,7 @@ class DAOs {
         @Query("SELECT * FROM activators")
         fun getAllActivators(): Flow<List<Activator>>
 
-        @Query("SELECT * FROM activators WHERE NOT userCancelled AND COALESCE(endAfterDate > strftime('%s', 'now'),1) AND endAfterRep > (SELECT COUNT(*) FROM executions WHERE activatorId = activatorId)")
+        @Query("SELECT * FROM activators WHERE NOT userCancelled AND COALESCE(endAfterDate > strftime('%s', 'now'),1) AND COALESCE(endAfterRep > (SELECT COUNT(*) FROM executions WHERE activatorId = activatorId),1)")
         fun getActiveActivators(): Flow<List<Activator>>
 
         @Query("SELECT * FROM activators WHERE activatorId = :activatorId")
