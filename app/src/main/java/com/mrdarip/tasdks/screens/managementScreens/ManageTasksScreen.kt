624e464fe49870422944f50097deffb4d75af12e
mrdarip <darioelas2005@gmail.com>
2024-07-20 11:16:53 +0200

    reimplemented accessing editTaskScreen from ManageTasksScreen

--- app/src/main/java/com/mrdarip/tasdks/screens/managementScreens/ManageTasksScreen.kt
+++ app/src/main/java/com/mrdarip/tasdks/screens/managementScreens/ManageTasksScreen.kt
@@ -9,24 +9,19 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.mrdarip.tasdks.composables.TwoButtonsListItem
-import com.mrdarip.tasdks.data.entity.Activator
 import com.mrdarip.tasdks.navigation.AppScreens
 import com.mrdarip.tasdks.screens.bottomBarScreens.viewModels.MainMenuState
 import com.mrdarip.tasdks.screens.bottomBarScreens.viewModels.MainMenuViewModel
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
 @Composable
 fun ManageTasksScreen(navController: NavController) {
@@ -48,22 +43,12 @@ fun ManageTasksBodyContent(navController: NavController, mainMenuViewModel: Main
                     title = task.name,
                     subTitle = task.comment ?: "No comment",
                     emoji = task.iconEmoji,
-                    primaryIcon = Icons.Filled.PlayArrow,
+                    primaryIcon = Icons.Filled.Edit,
                     secondaryIcon = Icons.Filled.Add,
                     onPrimaryClick = {
-                        mainMenuViewModel.viewModelScope.launch(Dispatchers.IO) {
-                            val activatorId = mainMenuViewModel.insertActivator(
-                                Activator(
-                                    comment = "Created for one time execution",
-                                    taskToActivateId = task.taskId,
-                                    endRep = 1
-                                )
-                            )
-
-                            withContext(Dispatchers.Main) {
-                                navController.navigate("${AppScreens.PlayActivator.route}/$activatorId")
-                            }
-                        }
+                        navController.navigate(
+                            "${AppScreens.EditTask.route}/${task.taskId}"
+                        )
                     },
                     onSecondaryClick = {
                         navController.navigate(
