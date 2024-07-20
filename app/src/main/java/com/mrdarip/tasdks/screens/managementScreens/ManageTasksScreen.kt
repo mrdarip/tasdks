063017ddfbca5ce2ade05cef7ab47a2c2ffca965
mrdarip <darioelas2005@gmail.com>
2024-07-20 22:23:20 +0200

    re-implemented creating one time activators from manage tasks screen (as removed   on 624e464)

--- app/src/main/java/com/mrdarip/tasdks/screens/managementScreens/ManageTasksScreen.kt
+++ app/src/main/java/com/mrdarip/tasdks/screens/managementScreens/ManageTasksScreen.kt
@@ -10,18 +10,24 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
-import com.mrdarip.tasdks.composables.TwoButtonsListItem
+import com.mrdarip.tasdks.composables.ThreeButtonsListItem
+import com.mrdarip.tasdks.data.entity.Activator
 import com.mrdarip.tasdks.navigation.AppScreens
 import com.mrdarip.tasdks.screens.bottomBarScreens.viewModels.MainMenuState
 import com.mrdarip.tasdks.screens.bottomBarScreens.viewModels.MainMenuViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 @Composable
 fun ManageTasksScreen(navController: NavController) {
@@ -39,12 +45,13 @@ fun ManageTasksBodyContent(navController: NavController, mainMenuViewModel: Main
             verticalArrangement = Arrangement.spacedBy(16.dp)
         ) {
             items(mainMenuState.activeTasks) { task ->
-                TwoButtonsListItem(
+                ThreeButtonsListItem(
                     title = task.name,
                     subTitle = task.comment ?: "No comment",
                     emoji = task.iconEmoji,
                     primaryIcon = Icons.Filled.Edit,
                     secondaryIcon = Icons.Filled.Add,
+                    tertiaryIcon = Icons.Filled.PlayArrow,
                     onPrimaryClick = {
                         navController.navigate(
                             "${AppScreens.EditTask.route}/${task.taskId}"
@@ -54,6 +61,21 @@ fun ManageTasksBodyContent(navController: NavController, mainMenuViewModel: Main
                         navController.navigate(
                             "${AppScreens.CreateActivator.route}/${task.taskId}"
                         )
+                    },
+                    onTertiaryClick = {
+                        mainMenuViewModel.viewModelScope.launch(Dispatchers.IO) {
+                            val activatorId = mainMenuViewModel.insertActivator(
+                                Activator(
+                                    comment = "Created for one time execution",
+                                    taskToActivateId = task.taskId,
+                                    endRep = 1
+                                )
+                            )
+
+                            withContext(Dispatchers.Main) {
+                                navController.navigate("${AppScreens.PlayActivator.route}/$activatorId")
+                            }
+                        }
                     },
                     onLiItemClick = {
                         //TODO: Implement navigating to task details
