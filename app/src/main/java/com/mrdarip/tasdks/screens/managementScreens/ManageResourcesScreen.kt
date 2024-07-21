16a6ddf0f804aaad44e4b3b0bbb764cbbe966e44
mrdarip <darioelas2005@gmail.com>
2024-07-21 22:58:03 +0200

    created viewModel for ManageResourcesScreen and a structure for it

--- app/src/main/java/com/mrdarip/tasdks/screens/managementScreens/ManageResourcesScreen.kt
+++ app/src/main/java/com/mrdarip/tasdks/screens/managementScreens/ManageResourcesScreen.kt
@@ -1,18 +1,33 @@
 package com.mrdarip.tasdks.screens.managementScreens
 
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
-import com.mrdarip.tasdks.screens.managementScreens.viewModels.ManageActivatorsViewModel
+import com.mrdarip.tasdks.navigation.AppScreens
+import com.mrdarip.tasdks.screens.managementScreens.viewModels.ManageResourcesState
+import com.mrdarip.tasdks.screens.managementScreens.viewModels.ManageResourcesViewModel
 
 
 @Composable
 fun ManageResourcesScreen(navController: NavController) {
-    val mainMenuViewModel = viewModel(modelClass = ManageActivatorsViewModel::class.java)
+    val mainMenuViewModel = viewModel(modelClass = ManageResourcesViewModel::class.java)
+    val mainMenuState = mainMenuViewModel.state
     ManageResourcesBodyContent(
         navController = navController,
+        mainMenuState = mainMenuState,
         viewModel = mainMenuViewModel,
     )
 }
@@ -20,7 +35,27 @@ fun ManageResourcesScreen(navController: NavController) {
 @Composable
 fun ManageResourcesBodyContent(
     navController: NavController,
-    viewModel: ManageActivatorsViewModel
+    mainMenuState: ManageResourcesState,
+    viewModel: ManageResourcesViewModel
 ) {
-    Text("Manage Resources Screen")
+    Box(modifier = Modifier.fillMaxSize()) {
+        LazyColumn(
+            modifier = Modifier.padding(0.dp, 16.dp),
+            contentPadding = PaddingValues(horizontal = 16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            items(mainMenuState.allActivators) { task ->
+
+            }
+        }
+
+        Button(
+            onClick = { navController.navigate(AppScreens.CreateTask.route) },
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(16.dp)
+        ) {
+            Text(text = "New Task")
+        }
+    }
 }
--- app/src/main/java/com/mrdarip/tasdks/screens/managementScreens/viewModels/ManageResourcesViewModel.kt
+++ app/src/main/java/com/mrdarip/tasdks/screens/managementScreens/viewModels/ManageResourcesViewModel.kt
@@ -1,2 +1,51 @@
-package com.mrdarip.tasdks.screens.viewModels
+package com.mrdarip.tasdks.screens.managementScreens.viewModels
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.mrdarip.tasdks.data.Graph
+import com.mrdarip.tasdks.data.TasdksRepository
+import com.mrdarip.tasdks.data.entity.Activator
+import com.mrdarip.tasdks.data.entity.Task
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+
+class ManageResourcesViewModel(
+    private val repository: TasdksRepository = Graph.repository
+) : ViewModel() {
+    var state by mutableStateOf(ManageResourcesState())
+        private set
+
+    init {
+        getActivators()
+        getActiveActivators()
+    }
+
+    private fun getActivators() {
+        viewModelScope.launch {
+            repository.activators.collectLatest {
+                state = state.copy(allActivators = it)
+            }
+        }
+    }
+
+    private fun getActiveActivators() {
+        viewModelScope.launch {
+            repository.activeActivators.collectLatest {
+                state = state.copy(activeActivators = it)
+            }
+        }
+    }
+
+    fun getTaskById(taskId: Long): Flow<Task> {
+        return repository.getTaskByIdAsFlow(taskId)
+    }
+}
+
+data class ManageResourcesState(
+    val allActivators: List<Activator> = emptyList(),
+    val activeActivators: List<Activator> = emptyList()
+)
