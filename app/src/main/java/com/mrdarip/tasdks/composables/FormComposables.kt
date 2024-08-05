d346ca0d891664227adeaafdd7bc28a18f05b0d6
mrdarip <darioelas2005@gmail.com>
2024-08-05 19:50:14 +0200

    created composable to facilitate creating number fields

--- app/src/main/java/com/mrdarip/tasdks/composables/FormComposables.kt
+++ app/src/main/java/com/mrdarip/tasdks/composables/FormComposables.kt
@@ -2,7 +2,6 @@ package com.mrdarip.tasdks.composables
 
 import android.icu.text.BreakIterator
 import android.text.format.DateFormat
-import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -41,10 +40,8 @@ import androidx.compose.material3.TextField
 import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -168,29 +165,12 @@ fun TaskFields(
             )
         }
         if (!task.isPlaylist) {
-            var tempWaitTime by remember {
-                mutableStateOf(
-                    task.waitTime.toString()
-                )
-            }
-            TextField(
-                value = task.waitTime.toString(),
-                onValueChange = {
-                    tempWaitTime = it
-                    if (it.toIntOrNull() != null || it.isBlank()) {
-                        Log.i("TaskFields", "waitTime: ${it.toIntOrNull() ?: 9999}")
-                        onTaskChange(
-                            task.copy(
-                                waitTime = it.toIntOrNull() ?: 0
-                            )
-                        )
-                    }
-                },
-                label = { Text("Wait time") },
+            NumberInput(
+                value = task.waitTime,
+                onValidValueChange = { onTaskChange(task.copy(waitTime = it)) },
+                label = "Wait time",
                 suffix = { Text("minutes") },
-                modifier = Modifier.fillMaxWidth(),
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-                isError = tempWaitTime.toIntOrNull() == null
+                placeholder = "Minutes to wait",
             )
 
             Row(
@@ -555,4 +535,24 @@ fun capitalized(text: String): String {
             Locale.ROOT
         ) else it.toString()
     }
+}
+
+@Composable
+fun NumberInput(
+    value: Int,
+    onValidValueChange: (Int) -> Unit,
+    label: String,
+    placeholder: String,
+    suffix: @Composable (() -> Unit)? = null,
+    modifier: Modifier = Modifier
+) {
+    TextField(
+        value = value.toString(),
+        onValueChange = { onValidValueChange(it.toIntOrNull() ?: 0) },
+        label = { Text(label) },
+        placeholder = { Text(placeholder) },
+        suffix = suffix,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        modifier = modifier
+    )
 }
