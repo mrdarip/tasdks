cef5207a23f3a8adbbc0ca4de287cd920b068bc8
mrdarip <darioelas2005@gmail.com>
2024-08-17 01:20:15 +0200

    converted appScreen to enum

--- app/src/main/java/com/mrdarip/tasdks/navigation/AppScreen.kt
+++ app/src/main/java/com/mrdarip/tasdks/navigation/AppScreen.kt
@@ -1,39 +1,18 @@
 package com.mrdarip.tasdks.navigation
 
-sealed class AppScreen(val route: String, val isEntityScreen: Boolean = false) {
-    data object FirstScreen : AppScreen("FirstScreen")
-    data object SecondScreen : AppScreen("SecondScreen")
-    data object ThirdScreen : AppScreen("ThirdScreen")
-    data object EditActivator : AppScreen("EditActivator")
-    data object ManageActivators : AppScreen("ManageActivators")
-    data object CreateActivator : AppScreen("CreateActivator", true)
-    data object CreateResource : AppScreen("CreateResource", true)
-    data object EditResource : AppScreen("EditResource")
-    data object ManageResources : AppScreen("ManageResources")
-    data object ManageExecutions : AppScreen("ManageExecutions")
-    data object EditTask : AppScreen("EditTask", true)
-    data object ManageTasks : AppScreen("ManageTasks")
-    data object CreateTask : AppScreen("CreateTask")
-    data object PlayActivator : AppScreen("PlayActivator")
-    companion object {
-        fun valueOf(lastScreen: String): AppScreen {
-            return when (lastScreen) {
-                "FirstScreen" -> FirstScreen
-                "SecondScreen" -> SecondScreen
-                "ThirdScreen" -> ThirdScreen
-                "EditActivator" -> EditActivator
-                "ManageActivators" -> ManageActivators
-                "CreateActivator" -> CreateActivator
-                "CreateResource" -> CreateResource
-                "EditResource" -> EditResource
-                "ManageResources" -> ManageResources
-                "ManageExecutions" -> ManageExecutions
-                "EditTask" -> EditTask
-                "ManageTasks" -> ManageTasks
-                "CreateTask" -> CreateTask
-                "PlayActivator" -> PlayActivator
-                else -> FirstScreen
-            }
-        }
-    }
+enum class AppScreen(val route: String, val isEntityScreen: Boolean = false) {
+    FirstScreen("FirstScreen", false),
+    SecondScreen("SecondScreen", false),
+    ThirdScreen("ThirdScreen", false),
+    EditActivator("EditActivator", true),
+    ManageActivators("ManageActivators", false),
+    CreateActivator("CreateActivator", true),
+    CreateResource("CreateResource", true),
+    EditResource("EditResource", true),
+    ManageResources("ManageResources", false),
+    ManageExecutions("ManageExecutions", false),
+    EditTask("EditTask", true),
+    ManageTasks("ManageTasks", false),
+    CreateTask("CreateTask", true),
+    PlayActivator("PlayActivator", true), ;
 }
