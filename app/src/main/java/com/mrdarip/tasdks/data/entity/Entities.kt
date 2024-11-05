1eef7c42cbc1b51fc6dc02ce6253cd256e20fe2a
mrdarip <darioelas2005@gmail.com>
2024-11-05 10:34:16 +0100

    added Kdoc for Task entity

--- app/src/main/java/com/mrdarip/tasdks/data/entity/Entities.kt
+++ app/src/main/java/com/mrdarip/tasdks/data/entity/Entities.kt
@@ -8,11 +8,21 @@ import androidx.room.PrimaryKey
 import androidx.room.Relation
 
 
+/**
+ * @param name the name of the task
+ * @param comment the comment of the task
+ * @param iconEmoji the emoji of the task
+ * @param archived if the task is archived
+ * @param createdTime the time the task was created in seconds since epoch (unix time)
+ * @param isPlaylist if the task is a playlist
+ * @param waitTime the time to wait before the task can be completed, in seconds
+ * @param allowParallelTasks if the task allows parallel tasks
+ */
 @Entity(tableName = "tasks")
 data class Task(
     @PrimaryKey(autoGenerate = true) val taskId: Long = 0,
-    var name: String = "",
-    val comment: String? = null, //iconEmoji can be null so later on you can know what tasks weren't given an emoji
+    var name: String,
+    val comment: String? = null,
     val iconEmoji: String? = null,
     val archived: Boolean = false,
     val createdTime: Double = System.currentTimeMillis() / 1000.0,
