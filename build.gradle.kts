5bc2d120a12c308bccb33caf6005a9759301a07d
mrdarip <darioelas2005@gmail.com>
2024-12-24 18:18:41 +0100

    returned to kotlin 1.9 as K2 update turned out unsuccessful

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,9 +5,9 @@ buildscript {
     }
 }
 
+
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
-    alias(libs.plugins.compose.compiler) apply false  // Added Compose Compiler plugin
     id("com.google.devtools.ksp") version "1.9.0-1.0.12" apply false
 }
